84f89178fb7fc2f56e3646a45716c01fb2c32ba1
Karol Belina <karolbelina@gmail.com>
2022-11-14 17:49:29 +0100

    Apply `shellcheck` to `scripts/test.sh`

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,21 +9,21 @@ DIM='\033[2m'
 CLEAR='\033[0m'
 
 function usage {
-    printf "Usage: $0 <FILES...> [-b | --bless] [-h | --help]\n"
+    echo "Usage: $0 <FILES...> [-b | --bless] [-h | --help]"
 }
 
 function transform {
     vim --clean -es \
         +'syntax on' \
         +'set syntax=none' `# Reset any auto-detected syntaxes e.g conf` \
-        +"source $(dirname $0)/highlights.vim" `# Ensure all spans are exported as defined in the syntax` \
-        +"source $(dirname $0)/../syntax/uxntal.vim" \
+        +"source $(dirname "$0")/highlights.vim" `# Ensure all spans are exported as defined in the syntax` \
+        +"source $(dirname "$0")/../syntax/uxntal.vim" \
         +'run! syntax/2html.vim' \
         +'%print' \
         +'qa!' \
         /dev/stdin \
-    | awk -f $(dirname $0)/extract.awk \
-    | python3 $(dirname $0)/transpose.py
+    | awk -f "$(dirname "$0")/extract.awk" \
+    | python3 "$(dirname "$0")/transpose.py"
 }
 
 input_files=()
@@ -33,7 +33,7 @@ do
     case $1 in
     -b|--bless) bless=t;;
     -h|--help)  usage; exit 0;;
-    *)          input_files+=($1);;
+    *)          input_files+=("$1");;
     esac
     shift
 done
@@ -41,44 +41,42 @@ done
 tests_count=0
 failed_count=0
 
-for file in ${input_files[@]}; do
-    if [ ! -f $file ]; then
+for file in "${input_files[@]}"; do
+    if [ ! -f "$file" ]; then
         continue
     fi
 
     if [[ $bless ]]; then
-        printf "${YELLOW}!${CLEAR} Blessing snapshot for $file\n"
-        cat $file | transform > $file.snap
-        rm -f $file.snap.new
+        echo -e "${YELLOW}!${CLEAR} Blessing snapshot for $file"
+        transform < "$file" > "$file.snap"
+        rm -f "$file.snap.new"
         continue
     fi
 
-    if [ ! -f $file.snap ]; then
-        printf "${YELLOW}!${CLEAR} No $file.snap to compare to. Run \`$0 $file --bless\` to generate it\n"
+    if [ ! -f "$file.snap" ]; then
+        echo -e "${YELLOW}!${CLEAR} No $file.snap to compare to. Run \`$0 $file --bless\` to generate it"
         continue
     fi
 
-    snap="$(cat $file | transform)"
+    snap="$(transform < "$file")"
     status=0
-    diff=$(echo "$snap" | diff $file.snap -) || status=$?
+    diff=$(echo "$snap" | diff "$file.snap" -) || status=$?
 
     if [ $status -ne 0 ]; then
-        printf "${RED}X${CLEAR} Test $file failed\n"
-        printf "${DIM}"
-        echo "$diff"
-        printf "${CLEAR}"
-        echo "$snap" > $file.snap.new
+        echo -e "${RED}X${CLEAR} Test $file failed"
+        echo -e "${DIM}$diff${CLEAR}"
+        echo "$snap" > "$file.snap.new"
         ((failed_count++))
     else
-        printf "${GREEN}✓${CLEAR} Test $file passed\n"
-        rm -f $file.snap.new
+        echo -e "${GREEN}✓${CLEAR} Test $file passed"
+        rm -f "$file.snap.new"
     fi
 
     ((tests_count++))
 done
 
 if [[ $failed_count != 0 ]]; then
-    printf "$failed_count/$tests_count tests failed\n"
-elif [ ! $bless ]; then
-    printf "All tests passed\n"
+    echo "$failed_count/$tests_count tests failed"
+elif [ ! "$bless" ]; then
+    echo "All tests passed"
 fi
